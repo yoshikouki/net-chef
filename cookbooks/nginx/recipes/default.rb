431327c0f113685c02e290c38696a0e42e5f78d5
yoshikouki <yoshikouki@pepabo.com>
2020-10-25 17:59:00 +0900

    nginx の設定ファイルを追加

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -16,3 +16,21 @@ service 'nginx' do
            reload: true
   action %i[enable start]
 end
+
+# テンプレート設定ファイルを使用する
+template 'nginx.conf' do
+  path '/etc/nginx/nginx.conf'
+  source 'nginx.conf.erb'
+  owner 'root'
+  group 'root'
+  mode 0644
+  notifies :reload, 'service[nginx]'
+end
+
+# バーチャルホストの設定を配置するディレクトリを作成
+directory '/etc/nginx/vhost.d' do
+  owner 'root'
+  group 'root'
+  mode 0755
+  recursive true
+end
